4b4cda45ecac95f7776fdb8dfefdc74b5556d820
adrixo <adrianvalrom@gmail.com>
2022-10-14 15:42:24 +0200

    feat: add board as hashmap (parsed from [][])

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,19 +1,39 @@
 package game;
 
+import java.util.HashMap;
+
 public class Game {
 
-    private final boolean[][] board;
+    private final HashMap<Coordinate, Boolean> board;
+    private final int boardSize;
 
     public Game(boolean[][] board) {
-
-        this.board = board;
+        this.boardSize = board[0].length;
+        this.board = booleanBoardToHashmap(board);
     }
 
     public void nextGen() {
+    }
 
+    private HashMap<Coordinate, Boolean> booleanBoardToHashmap(boolean[][] board) {
+        HashMap<Coordinate, Boolean> hashmapBoard = new HashMap<>();
+        for (int x = 0; x < boardSize; x++) {
+            for (int y = 0; y < boardSize; y++) {
+                hashmapBoard.put(new Coordinate(x, y), board[x][y]);
+            }
+        }
+        return hashmapBoard;
     }
 
     public boolean[][] getBoard() {
-        return board;
+        boolean[][] parsedBoard = new boolean[boardSize][boardSize];
+
+        for (int x = 0; x < boardSize; x++) {
+            for (int y = 0; y < boardSize; y++) {
+                boolean cellIsAlive = board.get(new Coordinate(x, y));
+                parsedBoard[x][y] = cellIsAlive;
+            }
+        }
+        return parsedBoard;
     }
 }
--- src/test/java/game/Gameshould.java
+++ src/test/java/game/Gameshould.java
@@ -3,6 +3,7 @@ package game;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Gameshould {
@@ -15,35 +16,36 @@ class Gameshould {
         boolean[][] emptyBoard = new boolean[10][10];
         game = new Game(emptyBoard);
         game.nextGen();
-        assertEquals(emptyBoard, game.getBoard());
+        assertThat(emptyBoard).isEqualTo(game.getBoard());
     }
 
     @Test public void
     keep_cells_alive_when_they_should_not_die() {
-        boolean[][] aliveCells = {
+        boolean[][] inputBoard = {
                 {false, true, false, false},
                 {true, false, true, false},
                 {true, false, true, false},
                 {false, true, false, false}
         };
-        game = new Game(aliveCells);
+        game = new Game(inputBoard);
         game.nextGen();
         game.nextGen();
         game.nextGen();
-        assertEquals(aliveCells, game.getBoard());
+        assertThat(inputBoard).isEqualTo(game.getBoard());
     }
 
     @Test public void
     cells_die_from_under_population() {
-        boolean[][] board = {
+        boolean[][] inputBoard = {
                 {false, false, false, false},
                 {true, false, false, false},
                 {false, false, false, true},
                 {false, false, true, false}
         };
         boolean[][] expectedBoard = new boolean[4][4];
-        game = new Game(board);
+        game = new Game(inputBoard);
         game.nextGen();
         assertEquals(expectedBoard, game.getBoard());
+        assertThat(inputBoard).isEqualTo(game.getBoard());
     }
 }
